54e91167f82d6feefc4bd790c125c361bc226082
Somanathpatil <patilsomanath292@gmail.com>
2025-08-16 22:00:30 +0530

    string in php

---
+++ string.php
@@ -0,0 +1,12 @@
+<?php
+$name = "Somanath";
+$age = 21;
+
+echo 'My name is '.$name.' and my age is '.$age;
+echo"<br/>";
+
+$str = "Hello my name is Somanath";
+$str1 = " I am Sgu Student";
+$str.= $str1;
+echo $str;
+?>
