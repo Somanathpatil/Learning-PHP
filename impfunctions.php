fd94337de7e4102d90a623c8cc6b8502c23a7bc7
Somanathpatil <patilsomanath292@gmail.com>
2025-08-17 11:14:34 +0530

    function in php

---
+++ impfunctions.php
@@ -0,0 +1,22 @@
+<?php
+// $str = "Hello my name is somanath";
+// echo strlen($str);
+
+// $var = true;
+// var_dump($var);
+
+// echo date("l");
+// echo date("d");
+// echo date("m");
+// echo date("M");
+
+// echo phpinfo();
+
+// $var = "abc";
+// echo is_string($var);
+
+// echo rand();
+
+$var ="This is my phone";
+echo substr($var,4);
+?>
